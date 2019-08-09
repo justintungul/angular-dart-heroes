e3fd3e5fca942542b787de8b3f32f5afbfd251ba
Justin Tungul <justin.tungul@gmail.com>
2019-08-08 19:31:46 -0700

    Multiple Components

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,22 +1,20 @@
 import 'package:angular/angular.dart';
-import 'package:angular_forms/angular_forms.dart';
 
-import 'hero.dart';
+import 'src/hero.dart';
+import 'src/mock_heroes.dart';
+
+import 'src/hero_component.dart';
 
 @Component(
   selector: 'my-app',
-  template: '''
-    <h1>{{title}}</h1>
-    <h2>{{hero.name}}</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
-  ''',
-  directives: [formDirectives],
+  templateUrl: 'app_component.html',
+  styleUrls: ['app_component.css'],
+  directives: [coreDirectives, HeroComponent],
 )
 class AppComponent {
   final title = 'Tour of Heroes';
-  Hero hero = Hero(1, 'Windstorm');
+  List<Hero> heroes = mockHeroes;
+  Hero selected;
+
+  void onSelect(Hero hero) => selected = hero;
 }
